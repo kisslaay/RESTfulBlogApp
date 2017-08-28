68a9f8fe400dfecf02d20f8b6f735f5ea7ccd7a8
kc <kislay.chandra2002@gmail.com>
2017-08-28 19:40:09 +0000

    First commit

---
+++ app.js
@@ -0,0 +1,60 @@
+var express =  require("express"),
+bodyParser  =  require("body-parser"),
+mongoose    =  require("mongoose"),
+app         =  express();
+
+mongoose.connect("mongodb://localhost/restful_blog_app");
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+
+var blogSchema = new mongoose.Schema({
+   title: String,
+   image: String,
+   body: String,
+   created: {type: Date, default: Date.now}
+});
+
+var Blog = mongoose.model("Blog", blogSchema);
+
+app.get("/", function(req, res) {
+  res.redirect("/blogs");  
+});
+
+app.get("/blogs/new", function(req, res) {
+    res.render("new");
+});
+
+app.get("/blogs", function(req, res) {
+    Blog.find({}, function (err, blogs) {
+        if(err){
+            console.log("ERROR!!");
+        } else {
+            res.render("index", {blogs: blogs});
+        }
+    });
+});
+
+app.post("/blogs", function(req, res) {
+   Blog.create(req.body.blog, function(err) {
+      if(err) {
+          res.render("new");
+      } else {
+          res.redirect("/blogs");
+      }
+   });
+});
+
+app.get("/blogs/:id", function(req, res) {
+    Blog.findById(req.params.id, function(err, foundBlog) {
+        if(err) {
+            res.redirect("/blogs");
+        } else {
+            res.render("show", {blog: foundBlog});
+        }
+    })
+});
+
+app.listen(process.env.PORT, process.env.IP, function() {
+   console.log("SERVER IS RUNNING"); 
+});
